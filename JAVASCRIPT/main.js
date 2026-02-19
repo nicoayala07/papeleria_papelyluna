e11fdd3d895ee1b14608b1b978a1bc7b1b791a8c
nicoayala07 <nicolasayni@unisabana.edu.co>
2026-02-19 07:17:35 -0500

    boton de agregar a carrito

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -7,11 +7,21 @@ export function renderProducts(productosArray) {
     const card = document.createElement("div");
 
     card.innerHTML = `
-      <img src="../img/${producto.image}" alt="${producto.nombre}">
-      <h3>${producto.nombre}</h3>
-      <p>$${producto.precio}</p>
+  <img src="../img/${producto.image}" alt="${producto.nombre}">
+  <h3>${producto.nombre}</h3>
+  <p>$${producto.precio}</p>
+  <button class="btn-add" data-id="${producto.id}">
+    Agregar al carrito
+  </button>
     `;
 
     productsContainer.appendChild(card);
+
+    const addButton = card.querySelector(".btn-add");
+
+addButton.addEventListener("click", () => {
+  console.log("Producto agregado:", producto.id);
+});
+
   });
 }
