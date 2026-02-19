30db78bc03e0ec66e4903f999e833b727eb7b3c5
nicoayala07 <nicolasayni@unisabana.edu.co>
2026-02-19 10:29:42 -0500

    boton DEFINITIVO

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,10 +1,9 @@
 import { productos } from "./data.js";
-import { agregarAlCarrito } from "./carrito.js";
 import { renderProducts } from "./main.js";
 
 renderProducts(productos);
 
 console.log("App conectada");
 
-export const carrito = [];
+
 
