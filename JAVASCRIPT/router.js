1012ecb566884bd0beec7be422fe2b521cca8085
Tomaspoveda <tomasposa@unisabana.edu.co>
2026-02-25 13:09:05 -0500

    vistas completas
    Agrege unas vainas en el css y quedo pleno

--- JAVASCRIPT/router.js
+++ JAVASCRIPT/router.js
@@ -1,27 +1,37 @@
 function navegarA(nombreVista){
     document.querySelectorAll(".vista").forEach(v => v.classList.remove("activa"));
 
-    document.getElementById("vista"+nombreVista).classList.add("activa");
+    document.getElementById("vista-"+nombreVista).classList.add("activa");
 
     document.querySelectorAll(".nav-btn").forEach(b => b.classList.remove("activa"));
     
-    const btnActiva= document.querySelector('[data-vista="${nombreVista}"]');
-    btnActiva.classList.add("activa");
+    const btnActiva= document.querySelector(`[data-vista="${nombreVista}"]`);
+    if (btnActiva) btnActiva.classList.add("activa"); 
+    
 }
-document.querySelectorAll(".nav-btn[data-vista]").forEach(btn => {
-    btn.addEventListener("click", () => {
-        navegarA(btn.dataset.vista);
+    document.querySelectorAll(".nav-btn[data-vista]").forEach(btn => {
+        btn.addEventListener("click", () => {
+            navegarA(btn.dataset.vista);
+        });
     });
-});
 
 document.getElementById("btn-ver-catalogo").addEventListener("click", () => {
     navegarA("ventas");})
-document,getElementById("btn-ver-historial").addEventListener("click", () => {
-    navegarA("carrito");})
+document.getElementById("btn-ver-historial").addEventListener("click", () => {
+    navegarA("historial");})
 document.getElementById("btn-ver-todo").addEventListener("click", () => {
-    navegarA("contacto");})
+    navegarA("ventas");})
 document.getElementById("btn-volver-historial").addEventListener("click", () => {
-    navegarA("contacto");})
+    navegarA("historial");})
 
-document.getElementById("btn-cerrar-buscardor").addEventListener("click", () => {
-    navegarA("ventas
+document.getElementById("btn-buscador").addEventListener("click", () => {
+    const overlay = document.getElementById("search-overlay");
+    overlay.classList.toggle("activa");
+    if (overlay.classList.contains("activa")) {
+        document.getElementById("search-input").focus();
+    }
+});
+
+document.getElementById("btn-cerrar-buscador").addEventListener("click", ()=> {
+    document.getElementById("search-overlay").classList.remove("activa");
+});
